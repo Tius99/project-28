9e2ec06ec36b3aa4fdd25b3f1a64828abb74a5e1
DEAD ICE <75473464+Tius99@users.noreply.github.com>
2021-01-23 14:06:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6;
+var stone1;
+var world,boy;
+var launcher1;
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1000,100,30);
+	mango3=new mango(1000,180,30);
+	mango4=new mango(1200,200,30);
+	mango5=new mango(900,220,30);
+	mango6=new mango(1100,200,30);
+
+	stone1 = new stone(100,100);
+	launcher1 = new launcher(stone.body,{x:100,y:100});
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+  stone1.display();	
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  launcher1.display();
+  groundObject.display();
+}
